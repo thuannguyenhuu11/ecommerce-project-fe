acc9ac5cedd4fdb178476285bc2ffc814c41339b
Thuan Nguyen <thuannguyenhuu11@gmail.com>
2024-12-05 23:23:39 +0700

    feat: create hero section

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,13 @@
 import Banner from './Banner';
 import Categories from './Categories';
+import HeroSection from './HeroSection';
 
 const Home = () => {
     return (
         <>
             <Banner />
             <Categories />
+            <HeroSection />
         </>
     );
 };
